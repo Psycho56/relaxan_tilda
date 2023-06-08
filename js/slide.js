6c6d0b7771e6e2fc3486d4162c0ef8df1f5fb642
kamoliddin <kuliyevkamoliddin5@gmail.com>
2023-06-08 13:18:36 +0500

    change

--- js/slide.js
+++ js/slide.js
@@ -1,15 +1,15 @@
-// let textList = document.querySelectorAll('.list');
-// let imageList = document.querySelectorAll('.hover-image');
+let textList = document.querySelectorAll('.list');
+let imageList = document.querySelectorAll('.hover-image');
 
-// textList.forEach((textItem) => {
-//   textItem.addEventListener('mouseover', (event) => {
-//     let id = event.target.id.split('-')[1];
-//     imageList.forEach((imageItem) => {
-//       imageItem.style.display = 'none';
-//     });
-// });
-//  document.getElementById(`image${id}`).style.display = 'block';
-// });
+textList.forEach((textItem) => {
+  textItem.addEventListener('mouseover', (event) => {
+    let id = event.target.id.split('-')[1];
+    imageList.forEach((imageItem) => {
+      imageItem.style.display = 'none';
+    });
+});
+ document.getElementById(`image${id}`).style.display = 'block';
+});
 
 
   
